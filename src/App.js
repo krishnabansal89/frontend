4c0a0a3499c99299b6f61c3d4f0a9e27cf6b31e7
krishnabansal89 <83087345+krishnabansal89@users.noreply.github.com>
2022-12-31 17:52:00 +0530

    final-hashrouter

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import Auth from './components/Auth'
 import Home from './components/Home'
 import Add from './components/Add'
 import View from './components/View'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter,HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
-      <Route exact path='/#' element ={<Home/>}></Route>
-      <Route exact path='/#/auth' element ={<Auth/>}></Route>
-      <Route  exact path='/#/add/:id' element ={<Add/>}></Route>
-      <Route exact path='/#/View' element ={<View/>}></Route>
+      <Route exact path='/' element ={<Home/>}></Route>
+      <Route exact path='/auth' element ={<Auth/>}></Route>
+      <Route  exact path='/add/:id' element ={<Add/>}></Route>
+      <Route exact path='/View' element ={<View/>}></Route>
     
     </Routes>
-   </BrowserRouter>
+   </HashRouter>
   )
 }
 
